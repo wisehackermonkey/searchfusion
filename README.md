23dce5991f56e40e68895e30a9d86af032b62f48
wisehackermonkey <oranbusiness@gmail.com>
2024-07-23 06:29:26 +0000

    fixed install instrctions

--- README.md
+++ README.md
@@ -4,6 +4,25 @@
 ## local gui app that allows you to search for images and text files by tags autogenrated by ml classifiers
 
 
+# install
+# install conda
+```bash
+cd ~
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+```
+# close and open terminal
+```bash
+conda --version
+conda create -n searchfusion python=3.8
+conda activate searchfusion
+pip install 'transformers[torch]' pillow
+```
+
+
 # examples of just using the classifer script
 ```bash
 python blip.py --text "A " --input_file ./images/muffin.png --verbose > classified_images.txt
--- blip.py
+++ blip.py
@@ -1,65 +1,10 @@
-# # import requests
-# # from PIL import Image
-# # from transformers import BlipProcessor, BlipForConditionalGeneration
-
-# # processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-# # model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-
-# # img_url = "./images/muffin.png"
-# # text = "A "
-
-
-
-# # raw_image = Image.open(img_url).convert('RGB')
-# # inputs = processor(raw_image, text, return_tensors="pt")
-# # out = model.generate(**inputs)
-
-
-# # print(processor.decode(out[0], skip_special_tokens=True),end="")
-
-# import argparse
-# import sys
-# import os
-# from PIL import Image
-# from transformers import BlipProcessor, BlipForConditionalGeneration
-
-# def main():
-#     parser = argparse.ArgumentParser(description='Image captioning using BLIP.')
-#     parser.add_argument('--text', type=str, default='A ', help='Text prompt for image captioning.')
-#     parser.add_argument('--input_file', type=str, default='-', help='Path to the input image file (default: stdin).')
-
-#     args = parser.parse_args()
-#     sys.stdout = open(os.devnull, 'w')
-#     sys.stderr = open(os.devnull, 'w')
-#     # Load the model and processor
-#     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-#     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-#     sys.stdout = sys.__stdout__
-#     # Read the image
-#     if args.input_file == '-':
-#         # Read image from stdin
-#         raw_image = Image.open(sys.stdin.buffer).convert('RGB')
-#     else:
-#         # Read image from the provided file path
-#         raw_image = Image.open(args.input_file).convert('RGB')
-
-#     # Process the image and generate caption
-#     inputs = processor(raw_image, args.text, return_tensors="pt")
-#     out = model.generate(**inputs)
-
-#     # Print the generated caption
-#     print(processor.decode(out[0], skip_special_tokens=True))
-
-# if __name__ == '__main__':
-#     main()
-
-
 import argparse
 import sys
 import os
 import logging
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
+
 
 def main():
     parser = argparse.ArgumentParser(description='Image captioning using BLIP.')
